2303a39954936a6859202df92d2a542a6eb3bc45
Chase <chase24@gmail.com>
2019-04-08 23:46:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var request = new XMLHttpRequest()
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://statsapi.web.nhl.com/api/v1/teams/14?expand=team.roster', true)
 
+
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
@@ -14,8 +15,8 @@ request.onload = function () {
   	var num = JSON.stringify(team[index].jerseyNumber).replace(/"/g,'');
   	var div = document.createElement("div");
   	var link = JSON.stringify(team[index].person.link).replace(/"/g,'');
+  	//window.alert('https://statsapi.web.nhl.com' + link)
 
-  	//div.innerHTML = '<a href="#" class="list-group-item">' + name + num + '<p></p></a>';
   	div.innerHTML='<a href="#" class="list-group-item" data-toggle="collapse" data-target="#sm" data-parent="#menu">' + name + ' <span class="label label-info">5</span> <span class="glyphicon glyphicon-envelope pull-right"></span></a>'
  	div.innerHTML=div.innerHTML + '<div id="sm" class="sublinks collapse">'
     div.innerHTML=div.innerHTML + '<a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span>' + num + ' </a>'
