89186cc50af5408e3d5c4ba5efd8eee339c0f846
Chase <chase24@gmail.com>
2019-04-08 22:47:22 -0400

    working to accomplish collapsable lists

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ request.onload = function () {
   var team=data.teams[0].roster.roster
 
   for (index = 0; index < team.length; ++index){
-  	//alert(JSON.stringify(team[index].person.fullName) + JSON.stringify(team[index].jerseyNumber))
-  	var name = JSON.stringify(team[index].person.fullName);
-  	var num = JSON.stringify(team[index].jerseyNumber)
+  	var name = JSON.stringify(team[index].person.fullName).replace(/"/g,'');
+  	var position = JSON.stringify(team[index].position.name).replace(/"/g,'');
+  	var num = JSON.stringify(team[index].jerseyNumber).replace(/"/g,'');
   	var div = document.createElement("div");
-  	//div.style.width = "100px";
-	//div.style.height = "100px";
-	//div.style.background = "red";
-	//div.style.color = "white";
-  	div.innerHTML = name + num;
-  	//div.outerHtml = "hello";
+  	var link = JSON.stringify(team[index].person.link).replace(/"/g,'');
 
+  	//div.innerHTML = '<a href="#" class="list-group-item">' + name + num + '<p></p></a>';
+  	div.innerHTML='<a href="#" class="list-group-item" data-toggle="collapse" data-target="#sm" data-parent="#menu">' + name + ' <span class="label label-info">5</span> <span class="glyphicon glyphicon-envelope pull-right"></span></a>'
+ 	div.innerHTML=div.innerHTML + '<div id="sm" class="sublinks collapse">'
+    div.innerHTML=div.innerHTML + '<a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span>' + num + ' </a>'
+    div.innerHTML=div.innerHTML + '<a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span>' + position + ' </a> </div>'
   	document.getElementById("main").appendChild(div);
   }
 }
