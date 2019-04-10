8043d2a3ca3e72fddfc1d2144000a656ba5dd8b4
Chase <chase24@gmail.com>
2019-04-10 19:09:13 -0400

    pulling player data

--- script_old.js
+++ script_old.js
@@ -7,25 +7,19 @@ var age =""
 var height = ""
 var weight = ""
 var nationality = ""
-var name = ""
-var position = ""
-var num = ""
-var div = ""
-var link = ""
-var data = ""
 
 
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
-  var team = data.teams[0].roster.roster
+  var team=data.teams[0].roster.roster
 
   for (index = 0; index < team.length; ++index){
-  	name = JSON.stringify(team[index].person.fullName).replace(/"/g,'');
-  	position = JSON.stringify(team[index].position.name).replace(/"/g,'');
-  	num = JSON.stringify(team[index].jerseyNumber).replace(/"/g,'');
-  	div = document.createElement("div");
-  	link = JSON.stringify(team[index].person.link).replace(/"/g,'');
+  	var name = JSON.stringify(team[index].person.fullName).replace(/"/g,'');
+  	var position = JSON.stringify(team[index].position.name).replace(/"/g,'');
+  	var num = JSON.stringify(team[index].jerseyNumber).replace(/"/g,'');
+  	var div = document.createElement("div");
+  	var link = JSON.stringify(team[index].person.link).replace(/"/g,'');
   	//window.alert('https://statsapi.web.nhl.com' + link)
 
 
@@ -40,14 +34,13 @@ request.onload = function () {
   			window.weight = JSON.stringify(myJson.people[0].weight);
   			nationality = JSON.stringify(myJson.people[0].nationality);
     		console.log(weight);
-        create_row(myJson)
 
   		});
 
   		console.log(weight)
   		//alert(weight)
 	  	div.innerHTML='<a href="#" class="list-group-item" data-toggle="collapse" data-target="#sm" data-parent="#menu">' + name + ' <span class="label label-info">' + num + '</span></a>'
-	 	  div.innerHTML=div.innerHTML + '<div id="sm" class="sublinks collapse">'
+	 	div.innerHTML=div.innerHTML + '<div id="sm" class="sublinks collapse">'
 	    div.innerHTML=div.innerHTML + '<a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span>' + age + ' </a>'
 	    div.innerHTML=div.innerHTML + '<a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span>' + height + ' </a>'
 	    div.innerHTML=div.innerHTML + '<a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span>' + weight + ' </a>'
@@ -55,12 +48,6 @@ request.onload = function () {
 	    div.innerHTML=div.innerHTML + '<a class="list-group-item small"><span class="glyphicon glyphicon-chevron-right"></span>' + position + ' </a> </div>'
   		document.getElementById("main").appendChild(div);
   }
-}
-
-function create_row(myJson)
-{
-  //alert(weight)
- // alert(name)
 }
 
 
