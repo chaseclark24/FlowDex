058435efc50af517d73c759e3677438355af4b6f
Chase <chase24@gmail.com>
2019-04-11 19:28:57 -0400

    site merge
    When you can't figure out how to link data between sites, just combine them.

--- scriptHome.js
+++ scriptHome.js
@@ -1,15 +1,3 @@
-fetch('https://statsapi.web.nhl.com/api/v1/teams/')
-  .then(function(response) {
-    //data1 = response.json();
-    return response.json();
-  })
-  .then(function(myJson){
-
-    //teamData = myJson.teams[0].roster.roster
-    console.log(JSON.stringify(myJson.teams))
-    //data1 = JSON.stringify(teamData[1].person.fullName).replace(/"/g,'');
-    //return teamData;
-  })
 
 
 fetch('https://statsapi.web.nhl.com/api/v1/teams/')
@@ -17,17 +5,25 @@ fetch('https://statsapi.web.nhl.com/api/v1/teams/')
     return response.json().then(data => data.teams);
 }).then(teams => {
       for(team of teams) {
-         //const {jerseyNumber, person:{fullName, link}, position:{name}} = team;
-         //console.log(team.id)
-
          div = document.createElement("div");
-         div.innerHTML='<a href="#" class="list-group-item" data-toggle="collapse" data-target="#sm" data-parent="#menu">' + team.name + '</span></a>'
-         document.getElementById("main").appendChild(div);
+         div.innerHTML='<a href="javascript:test(' + team.id + ');" class="list-group-item" data-toggle="collapse" data-target="#sm" data-parent="#menu">' + team.name + '</span></a>'
+         document.getElementById("teamList").appendChild(div);
 
 
          
       }
 });
 
+
+var teamList = document.getElementById('teamList');
+
+
+function test(hello){
+
+
+alert(hello)
+document.getElementById('teamList') = 'hidden';
+
+};
 
 
